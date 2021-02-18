35a81de35f9a7d7e3973f46f15c6988acdc233f6
Akansha Kumari <aks9727kumari@gmail.com>
2021-02-18 19:55:48 +0530

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2019 Online Book Reselling System |<a href="Admindetail.html" target="_blank"  > Designed by : A2S2Group</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
